709d451a1e9e8a8b765724b49c987f8a753ba737
lalitsingh <lalitbisalpur@gmail.com>
2018-03-21 11:42:15 +0530

    initial commints

--- app/src/main/java/hacker/l/venderapp/activity/DashboardActivity.java
+++ app/src/main/java/hacker/l/venderapp/activity/DashboardActivity.java
@@ -26,8 +26,10 @@ import hacker.l.venderapp.utilities.FontManager;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
-    TextView tv_help, tv_about, tv_changepass, tv_serviceZone, tv_vehicle, tv_driverslist, tv_home;
-    LinearLayout layout_help, layout_about, layout_chagepass, layout_servicezone, layout_vehiclelist, layout_driverList, layout_home;
+    TextView tv_information,tv_tripmanager,tv_settings,tv_drivres,tv_taxi,tv_city,tv_account,tv_toturial,tv_completeTrip,
+            tv_avality,tv_bids,tv_booking,tv_opnebooking;
+    LinearLayout layout_information ,layout_tripmanager,layout_settings,layout_driverList,layout_taxi,layout_city,
+            layout_completeTrip,layout_account,layout_toturial,layout_avality,layout_vids,layout_booking,layout_opnebooking;
     DrawerLayout drawer;
 
     @Override
@@ -49,40 +51,65 @@ public class DashboardActivity extends AppCompatActivity
 
     private void init() {
         Typeface nova = FontManager.getFontTypeface(this, "fonts/ProximaNova-Regular.otf");
-        layout_help = (LinearLayout) findViewById(R.id.layout_help);
-        layout_about = (LinearLayout) findViewById(R.id.layout_about);
-        layout_chagepass = (LinearLayout) findViewById(R.id.layout_chagepass);
-        layout_servicezone = (LinearLayout) findViewById(R.id.layout_servicezone);
-        layout_vehiclelist = (LinearLayout) findViewById(R.id.layout_vehiclelist);
+        layout_information = (LinearLayout) findViewById(R.id.layout_information);
+        layout_tripmanager = (LinearLayout) findViewById(R.id.layout_tripmanager);
+        layout_settings = (LinearLayout) findViewById(R.id.layout_settings);
         layout_driverList = (LinearLayout) findViewById(R.id.layout_driverList);
-        layout_home = (LinearLayout) findViewById(R.id.layout_home);
-        tv_help = (TextView) findViewById(R.id.tv_help);
-        tv_about = (TextView) findViewById(R.id.tv_about);
-        tv_changepass = (TextView) findViewById(R.id.tv_changepass);
-        tv_serviceZone = (TextView) findViewById(R.id.tv_serviceZone);
-        tv_vehicle = (TextView) findViewById(R.id.tv_vehicle);
-        tv_driverslist = (TextView) findViewById(R.id.tv_driverslist);
-        tv_home = (TextView) findViewById(R.id.tv_home);
-        tv_help.setTypeface(nova);
-        tv_about.setTypeface(nova);
-        tv_changepass.setTypeface(nova);
-        tv_serviceZone.setTypeface(nova);
-        tv_vehicle.setTypeface(nova);
-        tv_driverslist.setTypeface(nova);
-        tv_home.setTypeface(nova);
+        layout_taxi = (LinearLayout) findViewById(R.id.layout_taxi);
+        layout_driverList = (LinearLayout) findViewById(R.id.layout_driverList);
+        layout_city = (LinearLayout) findViewById(R.id.layout_city);
+        layout_account = (LinearLayout) findViewById(R.id.layout_account);
+        layout_toturial = (LinearLayout) findViewById(R.id.layout_toturial);
+        layout_avality = (LinearLayout) findViewById(R.id.layout_avality);
+        layout_completeTrip = (LinearLayout) findViewById(R.id.layout_completeTrip);
+        layout_vids = (LinearLayout) findViewById(R.id.layout_vids);
+        layout_booking = (LinearLayout) findViewById(R.id.layout_booking);
+        layout_opnebooking = (LinearLayout) findViewById(R.id.layout_opnebooking);
+        tv_information = (TextView) findViewById(R.id.tv_information);
+        tv_tripmanager = (TextView) findViewById(R.id.tv_tripmanager);
+        tv_settings = (TextView) findViewById(R.id.tv_settings);
+        tv_drivres = (TextView) findViewById(R.id.tv_drivres);
+        tv_taxi = (TextView) findViewById(R.id.tv_taxi);
+        tv_city = (TextView) findViewById(R.id.tv_city);
+        tv_account = (TextView) findViewById(R.id.tv_account);
+        tv_toturial = (TextView) findViewById(R.id.tv_toturial);
+        tv_completeTrip = (TextView) findViewById(R.id.tv_completeTrip);
+        tv_avality = (TextView) findViewById(R.id.tv_avality);
+        tv_bids = (TextView) findViewById(R.id.tv_bids);
+        tv_booking = (TextView) findViewById(R.id.tv_booking);
+        tv_opnebooking = (TextView) findViewById(R.id.tv_opnebooking);
+        tv_information.setTypeface(nova);
+        tv_tripmanager.setTypeface(nova);
+        tv_settings.setTypeface(nova);
+        tv_drivres.setTypeface(nova);
+        tv_taxi.setTypeface(nova);
+        tv_city.setTypeface(nova);
+        tv_account.setTypeface(nova);
+        tv_toturial.setTypeface(nova);
+        tv_completeTrip.setTypeface(nova);
+        tv_avality.setTypeface(nova);
+        tv_bids.setTypeface(nova);
+        tv_booking.setTypeface(nova);
+        tv_opnebooking.setTypeface(nova);
         setOnclick();
         HomeFragment homeFragment = HomeFragment.newInstance("", "");
         moveFragment(homeFragment);
     }
 
     private void setOnclick() {
-        layout_help.setOnClickListener(this);
-        layout_about.setOnClickListener(this);
-        layout_chagepass.setOnClickListener(this);
-        layout_servicezone.setOnClickListener(this);
-        layout_vehiclelist.setOnClickListener(this);
+        layout_information.setOnClickListener(this);
+        layout_tripmanager.setOnClickListener(this);
+        layout_settings.setOnClickListener(this);
         layout_driverList.setOnClickListener(this);
-        layout_home.setOnClickListener(this);
+        layout_taxi.setOnClickListener(this);
+        layout_city.setOnClickListener(this);
+        layout_account.setOnClickListener(this);
+        layout_toturial.setOnClickListener(this);
+        layout_completeTrip.setOnClickListener(this);
+        layout_avality.setOnClickListener(this);
+        layout_vids.setOnClickListener(this);
+        layout_booking.setOnClickListener(this);
+        layout_opnebooking.setOnClickListener(this);
     }
 
     @Override
@@ -123,19 +150,19 @@ public class DashboardActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -159,7 +186,7 @@ public class DashboardActivity extends AppCompatActivity
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.layout_home:
+            case R.id.layout_information:
                 HomeFragment homeFragment = HomeFragment.newInstance("", "");
                 moveFragment(homeFragment);
                 navHide();
